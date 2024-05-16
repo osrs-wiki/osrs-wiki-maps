a929ae84d1ec31a2258dc8309a4aaf6fe4b365a2
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2024-05-16 16:57:00 -0700

    implement numpy and opencv stitching
    sheesh

--- scripts/buildCompositeImage.py
+++ scripts/buildCompositeImage.py
@@ -1,66 +1,68 @@
 ### Build a large image out of the region tiles produced by RuneLite's image dumper
 import glob
 import os
-from PIL import Image
 import time
 from memory_profiler import profile, memory_usage
-import multiprocessing.dummy
+import multiprocessing
+import cv2
+import numpy as np
 
-# Pyvips on windows is finnicky
-# Windows binaries are required: https://pypi.org/project/pyvips/, https://www.libvips.org/install.html
-LIBVIPS_VERSION = "8.15"
-vipsbin = os.path.join(os.getcwd(), f"vipsbin/vips-dev-{LIBVIPS_VERSION}/bin")
-os.environ['PATH'] = os.pathsep.join((vipsbin, os.environ['PATH']))
-import pyvips as pv
+### Configure this before running the script
+VERSION = "2024-04-10_a"
+regionPath = f"./osrs-wiki-maps/out/mapgen/versions/{VERSION}/tiles/base"
+OUTPUT_PATH = f"./osrs-wiki-maps/out/mapgen/versions/{VERSION}/composites"
+REGION_TILE_LENGTH = 64
+TILE_PIXEL_LENGTH = 4
+REGION_PIXEL_LENGTH = REGION_TILE_LENGTH * TILE_PIXEL_LENGTH
+MAX_MAP_SIDE_LENGTH = 999 # in regions
+PLANE_COUNT = 4
+
+# Identify files produced by the dumper
+fileType = "/*.png"
+regionImageFilePaths = [os.path.normpath(path) for path in glob.glob(f"{regionPath}{fileType}")]
+
+# Define empty region appearance
+emptyRegion = np.zeros((REGION_PIXEL_LENGTH, REGION_PIXEL_LENGTH, 3), dtype=np.uint8)
 
 @profile
+def assemblePlanes(plane, upperX, upperY, lowerX, lowerY):
+	imageRows = list()
+	for regionY in range(upperY, lowerY-1, -1):
+		imageRow = list()
+		for regionX in range(lowerX, upperX+1, 1):
+			targetRegionFileName = os.path.normpath(os.path.join(regionPath, f"{plane}_{regionX}_{regionY}.png"))
+			# If the region image exists, load it in
+			if targetRegionFileName in regionImageFilePaths:
+				imageRow.append(cv2.imread(targetRegionFileName, cv2.IMREAD_COLOR))
+			else:
+				imageRow.append(emptyRegion)
+		# Merge horizontally
+		row = np.hstack(imageRow)
+		imageRows.append(row)
+	# Merge verticall
+	image = np.vstack(imageRows)
+	cv2.imwrite(os.path.join(OUTPUT_PATH, f"plane_{plane}.png"), image)
+
 def buildCompositeImage():
-	### Configure this before running the script
-	VERSION = "2024-04-10_a"
-	regionPath = f"./osrs-wiki-maps/out/mapgen/versions/{VERSION}/tiles/base"
-	OUTPUT_PATH = f"./osrs-wiki-maps/out/mapgen/versions/{VERSION}/composites"
-	REGION_TILE_LENGTH = 64
-	TILE_PIXEL_LENGTH = 4
-	MAX_MAP_SIDE_LENGTH = 999 # in regions
-	PLANE_COUNT = 4
-
-	# Identify files produced by the dumper
-	fileType = "/*.png"
-	regionImageFilePaths = [os.path.normpath(path).replace("\\","/") for path in glob.glob(f"{regionPath}{fileType}")]
-
 	# Range the image dimensions
 	lowerX = lowerY = MAX_MAP_SIDE_LENGTH
 	upperX = upperY = 0
 	for regionFilePath in regionImageFilePaths:
 		fileName = os.path.splitext(os.path.basename(regionFilePath))[0]
-		plane, x, y = map(int, fileName.split("_")) # Expecting {plane}_{x}_{y}
+		_, x, y = map(int, fileName.split("_")) # Expecting {plane}_{x}_{y}
 		lowerX = min(lowerX, x)
 		lowerY = min(lowerY, y)
 		upperX = max(upperX, x)
 		upperY = max(upperY, y)
 
-	def assemblePlane(plane):
-		print(f"Assembling plane {plane}: {time.time()-startTime}")
-		# Load in the plane's region images
-		regionArray = list()
-		for regionY in range(upperY, lowerY-1, -1):
-			for regionX in range(lowerX, upperX+1, 1):			
-				targetRegionFileName = os.path.normpath(os.path.join(regionPath, f"{plane}_{regionX}_{regionY}.png")).replace("\\", "/")
-				# If we have an image already just load it in
-				if targetRegionFileName in regionImageFilePaths:
-					regionArray.append(pv.Image.new_from_file(targetRegionFileName))
-				else:
-					# Otherwise, provide a blank image
-					regionArray.append(pv.Image.black(REGION_TILE_LENGTH * TILE_PIXEL_LENGTH, REGION_TILE_LENGTH * TILE_PIXEL_LENGTH))
-		# Join the region images and write the result to file
-		planeImage = pv.Image.arrayjoin(regionArray, across=(upperX-lowerX+1))
-		planeImage.write_to_file(os.path.join(OUTPUT_PATH, f"plane_{plane}.png"))
+	planeRenderArgs = list()
+	for plane in range(0, PLANE_COUNT):
+		# assemblePlanes(plane, upperX, upperY, lowerX, lowerY)
+		planeRenderArgs.append((plane, upperX, upperY, lowerX, lowerY))
 
 	# Assign one core per plane image
-	pool = multiprocessing.dummy.Pool(PLANE_COUNT)
-	pool.map(assemblePlane, range(0, PLANE_COUNT))
-	pool.close()
-	pool.join()
+	with multiprocessing.Pool() as pool:
+		pool.starmap(assemblePlanes, planeRenderArgs)
 
 if __name__ == "__main__":
 	startTime = time.time()
